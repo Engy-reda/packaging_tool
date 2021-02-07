a9866f347fc1c51567a682c5a5d22b19906f8ebf
Engy Reda <engy.reda.s333@gmail.com>
2021-02-07 11:34:28 -0800

    webpack-packaging

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import hello from './hellow-world.js';
+hello()
+import './component/header/header'
+import './component/test/component.js';
+import './style.css';
+
+
+import pic from'./assets/c.jpg'
+import photo from './assets/image.jpg'
+const img = document.createElement("img");
+img.src=pic;
+document.body.appendChild(img);
